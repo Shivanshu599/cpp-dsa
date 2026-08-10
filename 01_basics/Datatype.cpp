923fab307491629acdc8304d445c3e14473ff5ce
Shivanshu599 <sharmashivanshu599@gmail.com>
2026-08-10 18:10:29 +0530

    Data types in c++

---
+++ 01_basics/Datatype.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Integer data type
+    int age = 19;
+
+    // Floating-point data type
+    float height = 5.8f;
+
+    // Double data type
+    double pi = 3.1415926535;
+
+    // Character data type
+    char grade = 'A';
+
+    // Boolean data type
+    bool isStudent = true;
+
+    // String data type
+    string name = "Shiva";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
