0b944509cd24285faf46c4392b3d6d8a16c51dca
Shivanshu599 <sharmashivanshu599@gmail.com>
2026-08-11 00:01:03 +0530

    Switch condition

---
+++ 01_basics/Switch.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int day;
+
+    cout << "Enter a number (1-7): "; //switch statement is used to prepare a menu of options for the uset to leaarn 
+
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "Monday";
+            break;
+
+        case 2:
+            cout << "Tuesday";
+            break;
+
+        case 3:
+            cout << "Wednesday";
+            break;
+
+        case 4:
+            cout << "Thursday";
+            break;
+
+        case 5:
+            cout << "Friday";
+            break;
+
+        case 6:
+            cout << "Saturday";
+            break;
+
+        case 7:
+            cout << "Sunday";
+            break;
+
+        default:
+            cout << "Invalid day";
+    }
+
+    return 0;
+}
